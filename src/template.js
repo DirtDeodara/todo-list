e7939e266aaea6ab5286f1c2936624d1499281d4
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-20 17:40:28 -0700

    passed tdd

--- src/template.js
+++ src/template.js
@@ -1,8 +1,12 @@
 function template(task) {
+    let checked = '';
+    if(task.completed) {
+        checked = 'checked';
+    }
     const html = /*html*/`
     <li>
         <p>${task.task}</p>
-        <input class="checkbox" type="checkbox" name="completed">
+        <input class="checkbox" type="checkbox" name="completed" ${checked}>
     </li>
     `;
     return html;
--- test/template.test.js
+++ test/template.test.js
@@ -3,7 +3,7 @@ import template from '../src/template.js';
 const test = QUnit.test;
 QUnit.module('template');
 
-test('test to return html', assert => {
+test('test to return html and competed as true', assert => {
     // Arrange
     const task = {
         
@@ -13,7 +13,27 @@ test('test to return html', assert => {
     const expected = /*html*/`
     <li>
         <p>Create a static example of your todo list</p>
-        <input class="checkbox" type="checkbox" name="completed">
+        <input class="checkbox" type="checkbox" name="completed" checked>
+    </li>
+    `;
+    // Act
+    const html = template(task);
+
+    // Assert
+    assert.htmlEqual(html, expected);
+});
+
+test('test to return html and competed as false', assert => {
+    // Arrange
+    const task = {
+        
+        task: 'Create a static example of your todo list',
+        completed: false
+    };
+    const expected = /*html*/`
+    <li>
+        <p>Create a static example of your todo list</p>
+        <input class="checkbox" type="checkbox" name="completed" >
     </li>
     `;
     // Act
